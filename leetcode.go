fc7a581a831b14c0648201928071e4329afd672c
Will Madison <wmadisondev@gmail.com>
2020-03-20 17:41:33 -0400

    solves number-of-islands problem

--- leetcode.go
+++ leetcode.go
@@ -2,6 +2,7 @@ package leetcode
 
 import (
 	"bytes"
+	"errors"
 	"math"
 	"sort"
 )
@@ -175,4 +176,160 @@ func hasMore(lists []*ListNode) bool {
 	}
 
 	return false
+}
+
+var errAllLandVisited = errors.New("AllLandVisited")
+
+type coordinate struct {
+	row, col int
+}
+
+type island struct {
+	coordinates []coordinate
+}
+
+// https://leetcode.com/problems/number-of-islands/
+func numIslands(grid [][]byte) int {
+	islands := discoverIslands(grid)
+	return len(islands)
+}
+
+func discoverIslands(grid [][]byte) []island {
+	var islands []island
+
+	visited := map[coordinate]struct{}{}
+
+	unvisitedSpot, err := findFirstUnvisitedLandMass(grid, visited)
+
+	for err != errAllLandVisited {
+		coordinates := discoverContiguousLand(unvisitedSpot, grid, visited)
+		islands = append(islands, island{coordinates: coordinates})
+		markVisited(coordinates, visited)
+		unvisitedSpot, err = findFirstUnvisitedLandMass(grid, visited)
+	}
+
+	return islands
+}
+
+func findFirstUnvisitedLandMass(grid [][]byte, seen map[coordinate]struct{}) (coordinate, error) {
+	for row := 0; row < len(grid); row++ {
+		for col := 0; col < len(grid[row]); col++ {
+			c := coordinate{row, col}
+
+			isLand := grid[row][col] == '1'
+			_, visited := seen[c]
+
+			if isLand && !visited {
+				return c, nil
+			}
+		}
+	}
+
+	return coordinate{}, errAllLandVisited
+}
+
+func markVisited(coordinates []coordinate, visited map[coordinate]struct{}) {
+	for _, c := range coordinates {
+		visited[c] = struct{}{}
+	}
+}
+
+type queue interface {
+	enqueue(coordinate)
+	dequeue() (coordinate, error)
+	peek() (coordinate, error)
+	empty() bool
+}
+
+type coordinateQueue struct {
+	data []coordinate
+	size int
+}
+
+func (q *coordinateQueue) enqueue(value coordinate) {
+	q.data = append(q.data, value)
+	q.size++
+}
+
+func (q *coordinateQueue) dequeue() (coordinate, error) {
+	if q.size > 0 {
+		value := q.data[0]
+		q.size--
+		q.data = q.data[1:]
+
+		return value, nil
+	}
+
+	return coordinate{}, errors.New("No Such Element")
+}
+
+func (q *coordinateQueue) peek() (coordinate, error) {
+	if q.size > 0 {
+		value := q.data[0]
+		return value, nil
+	}
+
+	return coordinate{}, errors.New("No Such Element")
+}
+
+func (q coordinateQueue) empty() bool {
+	return q.size == 0
+}
+
+func newCoordinateQueue() queue {
+	return &coordinateQueue{data: []coordinate{}}
+}
+
+func discoverContiguousLand(c coordinate, grid [][]byte, seen map[coordinate]struct{}) []coordinate {
+	var coordinates []coordinate
+
+	q := newCoordinateQueue()
+
+	q.enqueue(c)
+
+	for !q.empty() {
+		current, _ := q.dequeue()
+		seen[current] = struct{}{}
+
+		coordinates = append(coordinates, current)
+
+		hasLandAbove := current.row-1 >= 0 && grid[current.row-1][current.col] == '1'
+		hasLandRight := current.col+1 < len(grid[c.row]) && grid[current.row][current.col+1] == '1'
+		hasLandBelow := current.row+1 < len(grid) && grid[current.row+1][current.col] == '1'
+		hasLandLeft := current.col-1 >= 0 && grid[current.row][current.col-1] == '1'
+
+		if hasLandAbove {
+			above := coordinate{current.row - 1, current.col}
+			if _, visited := seen[above]; !visited {
+				seen[above] = struct{}{}
+				q.enqueue(above)
+			}
+		}
+
+		if hasLandRight {
+			right := coordinate{current.row, current.col + 1}
+			if _, visited := seen[right]; !visited {
+				seen[right] = struct{}{}
+				q.enqueue(right)
+			}
+		}
+
+		if hasLandBelow {
+			below := coordinate{current.row + 1, current.col}
+			if _, visited := seen[below]; !visited {
+				seen[below] = struct{}{}
+				q.enqueue(below)
+			}
+		}
+
+		if hasLandLeft {
+			left := coordinate{current.row, current.col - 1}
+			if _, visited := seen[left]; !visited {
+				seen[left] = struct{}{}
+				q.enqueue(left)
+			}
+		}
+	}
+
+	return coordinates
 }
