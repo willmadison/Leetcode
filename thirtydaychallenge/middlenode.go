acc65c6efe7f2f18f0afe01e94d8bd4076aabf15
Will Madison <wmadisondev@gmail.com>
2020-04-08 08:18:17 -0400

    solves Day 8 (Middle Node)

---
+++ thirtydaychallenge/middlenode.go
@@ -0,0 +1,40 @@
+package thirtydaychallenge
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	var middle *ListNode
+	var fast *ListNode
+	slow := head
+
+	if slow.Next == nil {
+		return head
+	}
+
+	if slow.Next.Next == nil {
+		middle = slow.Next
+	} else {
+		fast = slow.Next.Next
+	}
+
+	for middle == nil {
+		slow = slow.Next
+
+		if fast.Next != nil {
+			fast = fast.Next.Next
+		} else {
+			middle = slow
+			break
+		}
+
+		switch {
+		case fast == nil || fast.Next == nil:
+			middle = slow.Next
+		}
+	}
+
+	return middle
+}
