506cf47044a2b509f57ed963429bad16513b058d
Will Madison <wmadisondev@gmail.com>
2020-04-10 10:28:49 -0400

    passes Day 10 (Min Stack)

---
+++ thirtydaychallenge/minstack_test.go
@@ -0,0 +1,21 @@
+package thirtydaychallenge
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMinStack(t *testing.T) {
+	s := Constructor()
+	s.Push(1)
+	assert.Equal(t, 1, s.GetMin())
+	s.Push(0)
+	assert.Equal(t, 0, s.GetMin())
+	s.Push(-1)
+	s.Push(0)
+	assert.Equal(t, -1, s.GetMin())
+	s.Pop()
+	s.Pop()
+	assert.Equal(t, 0, s.GetMin())
+}
