73cfd3ddb5b2115b219ab063598933b5fdb6f503
willmadison <wmadisonDev@gmail.com>
2023-12-11 17:14:54 -0500

    Solves "Count Tested Devices", "Climb Stairs", and "Longest Palindrome"

--- kotlin/leetcode/src/main/kotlin/com/willmadison/leetcode/Solution.kt
+++ kotlin/leetcode/src/main/kotlin/com/willmadison/leetcode/Solution.kt
@@ -1,5 +1,8 @@
 package com.willmadison.leetcode
 
+import java.lang.Integer.max
+import kotlin.math.pow
+
 class Solution : VersionControl() {
 
     // https://leetcode.com/problems/first-bad-version
@@ -35,6 +38,113 @@ class Solution : VersionControl() {
         }
 
         return true
+    }
+
+    fun countTestedDevices(batteryPercentages: IntArray): Int {
+        var testedDevices = 0
+
+        for (i in batteryPercentages.indices) {
+            if (batteryPercentages[i] > 0) {
+                testedDevices++
+
+                for (j in (i + 1)..<batteryPercentages.size) {
+                    batteryPercentages[j] = max(batteryPercentages[j] - 1, 0)
+                }
+            }
+        }
+
+        return testedDevices
+    }
+
+    fun getGoodIndices(variables: Array<IntArray>, target: Int): List<Int> {
+        val goodIndices = mutableListOf<Int>()
+
+        for (i in variables.indices) {
+            if (variables[i].isGood(target)) {
+                goodIndices.add(i)
+            }
+        }
+
+        return goodIndices
+    }
+
+    private fun IntArray.isGood(target: Int): Boolean {
+        val a = this[0]
+        val b = this[1]
+        val c = this[2]
+        val m = this[3]
+
+        return (a.toDouble().pow(b).toInt() % 10).toDouble().pow(c).toInt() % m == target
+    }
+
+    fun countSubarrays(nums: IntArray, k: Int): Long {
+        var subarrays = 0L
+
+        val max = nums.max()
+
+        if (nums.count { it == max } >= k) {
+            subarrays++
+        }
+
+        var start = 0
+        var end = 0
+
+        while (start <= end) {
+            val countInWindow = nums.slice(start..end).count { it == max }
+            if (countInWindow >= k) {
+                subarrays++
+                start++
+            } else {
+                end++
+            }
+        }
+
+        return subarrays
+    }
+
+    private val memo = mutableMapOf<Int, Int>()
+
+    fun climbStairs(n: Int): Int {
+        if (n <= 3) {
+            return n
+        }
+
+        memo[1] = 1
+        memo[2] = 2
+        memo[3] = 3
+
+        for (i in 4..n) {
+            memo[i] = memo[i - 2]!! + memo[i - 1]!!
+        }
+
+        return memo[n]!!
+    }
+
+    fun longestPalindrome(value: String): Int {
+        var length = 0
+        val occurrencesByCharacter = value.groupingBy { it }.eachCount().toMutableMap()
+
+        var mostFrequentCharacterEntry = occurrencesByCharacter.filter { it.value == occurrencesByCharacter.values.max() }
+
+        while (mostFrequentCharacterEntry.all { it.value > 1 }) {
+            for (entry in mostFrequentCharacterEntry) {
+                if (entry.value % 2 == 0) {
+                    length += entry.value
+                    occurrencesByCharacter[entry.key] = 0
+                } else {
+                    length += entry.value - 1
+                    occurrencesByCharacter[entry.key] = 1
+                }
+
+                mostFrequentCharacterEntry = occurrencesByCharacter.filter { it.value == occurrencesByCharacter.values.max() }
+            }
+        }
+
+        if (mostFrequentCharacterEntry.any { it.value == occurrencesByCharacter.values.max() && it.value == 1 }) {
+            length++
+        }
+
+        return length
     }
 
 }
--- kotlin/leetcode/src/test/kotlin/com/willmadison/leetcode/SolutionTests.kt
+++ kotlin/leetcode/src/test/kotlin/com/willmadison/leetcode/SolutionTests.kt
@@ -23,6 +23,46 @@ class SolutionTests {
                     Arguments.of("aa", "aab", true),
             )
         }
+
+        @JvmStatic
+        fun testedDeviceProvider(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of(intArrayOf(1, 1, 2, 1, 3), 3),
+                    Arguments.of(intArrayOf(0, 1, 2), 2),
+            )
+        }
+
+        @JvmStatic
+        fun variablesProvider(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of(arrayOf(intArrayOf(2, 3, 3, 10), intArrayOf(3, 3, 3, 1), intArrayOf(6, 1, 1, 4)), 2, listOf<Int>(0, 2)),
+                    Arguments.of(arrayOf(intArrayOf(39, 3, 1000, 1000)), 17, listOf<Int>()),
+            )
+        }
+
+        @JvmStatic
+        fun subarraysProvider(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of(intArrayOf(1, 3, 2, 3, 3), 2, 6L),
+                    Arguments.of(intArrayOf(1, 4, 2, 1), 3, 0L),
+            )
+        }
+
+        @JvmStatic
+        fun stairProvider(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of(2, 2),
+                    Arguments.of(3, 3),
+            )
+        }
+
+        @JvmStatic
+        fun palindromeProvider(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of("abccccdd", 7),
+                    Arguments.of("a", 1),
+            )
+        }
     }
 
     @BeforeEach
@@ -46,6 +86,41 @@ class SolutionTests {
     @MethodSource("magazineSourceProvider")
     fun canConstruct(note: String, magazine: String, expected: Boolean) {
         val actual = solution.canConstruct(note, magazine)
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest(name = "countTestedDevices({0}) = {1}")
+    @MethodSource("testedDeviceProvider")
+    fun countTestedDevices(percentages: IntArray, expected: Int) {
+        val actual = solution.countTestedDevices(percentages)
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest(name = "getGoodIndices({0}, {1}) = {2}")
+    @MethodSource("variablesProvider")
+    fun getGoodIndicies(variables: Array<IntArray>, target: Int, expected: List<Int>) {
+        val actual = solution.getGoodIndices(variables, target)
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest(name = "countSubarrays({0}, {1}) = {2}")
+    @MethodSource("subarraysProvider")
+    fun countSubarrays(nums: IntArray, k: Int, expected: Long) {
+        val actual = solution.countSubarrays(nums, k)
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest(name = "climbStairs({0}) = {1}")
+    @MethodSource("stairProvider")
+    fun climbStairs(numStairs: Int, expected: Int) {
+        val actual = solution.climbStairs(numStairs)
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest(name = "longestPalindrome({0}) = {1}")
+    @MethodSource("palindromeProvider")
+    fun longestPalindrome(value: String, expected: Int) {
+        val actual = solution.longestPalindrome(value)
         assertEquals(expected, actual)
     }
 
