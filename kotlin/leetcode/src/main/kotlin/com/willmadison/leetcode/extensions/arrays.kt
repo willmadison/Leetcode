e30ccd7a01335653c2bea51b7ef15966491fcd79
willmadison <wmadisonDev@gmail.com>
2025-07-13 14:38:19 -0400

    solves another POTD

--- kotlin/leetcode/src/main/kotlin/com/willmadison/leetcode/extensions/arrays.kt
+++ kotlin/leetcode/src/main/kotlin/com/willmadison/leetcode/extensions/arrays.kt
@@ -2923,3 +2923,77 @@ fun maxFreeTime2(eventTime: Int, startTime: IntArray, endTime: IntArray): Int {
 
     return maximumFreeTime
 }
+
+fun mostBooked(n: Int, meetings: Array<IntArray>): Int {
+    val meetingCount = IntArray(n)
+    val usedRooms = PriorityQueue(Comparator { a: LongArray?, b: LongArray? ->
+        if (a!![0] != b!![0]) a[0].compareTo(b[0]) else a[1].compareTo(b[1])
+    })
+    val unusedRooms = PriorityQueue<Int?>()
+
+    for (i in 0..<n) {
+        unusedRooms.offer(i)
+    }
+
+    Arrays.sort(
+        meetings,
+        Comparator { a: IntArray?, b: IntArray? ->
+            if (a!![0] != b!![0]) a[0].compareTo(b[0]) else Integer.compare(a[1], b[1])
+        })
+
+    for (meeting in meetings) {
+        val start = meeting[0]
+        val end = meeting[1]
+
+        while (!usedRooms.isEmpty() && usedRooms.peek()!![0] <= start) {
+            val room = usedRooms.poll()!![1].toInt()
+            unusedRooms.offer(room)
+        }
+
+        if (!unusedRooms.isEmpty()) {
+            val room: Int = unusedRooms.poll()!!
+            usedRooms.offer(longArrayOf(end.toLong(), room.toLong()))
+            meetingCount[room]++
+        } else {
+            val roomAvailabilityTime = usedRooms.peek()!![0]
+            val room = usedRooms.poll()!![1].toInt()
+            usedRooms.offer(longArrayOf(roomAvailabilityTime + end - start, room.toLong()))
+            meetingCount[room]++
+        }
+    }
+
+    var maxMeetingCount = 0
+    var maxMeetingCountRoom = 0
+    for (i in 0..<n) {
+        if (meetingCount[i] > maxMeetingCount) {
+            maxMeetingCount = meetingCount[i]
+            maxMeetingCountRoom = i
+        }
+    }
+
+    return maxMeetingCountRoom
+}
+
+fun matchPlayersAndTrainers(players: IntArray, trainers: IntArray): Int {
+    Arrays.sort(players)
+    Arrays.sort(trainers)
+
+    val numPlayers = players.size
+    val numTrainers = trainers.size
+    var matches = 0
+
+    var playerIndex = 0
+    var trainerIndex = 0
+
+    while (playerIndex < numPlayers && trainerIndex < numTrainers) {
+        if (players[playerIndex] <= trainers[trainerIndex]) {
+            matches++
+            playerIndex++
+            trainerIndex++
+        } else {
+            trainerIndex++
+        }
+    }
+
+    return matches
+}
